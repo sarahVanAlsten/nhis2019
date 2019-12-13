c47e9bc830a1d14db29c0726695672442d454118
sarahVanAlsten <sarah.vanalsten@wustl.edu>
2019-12-13 14:58:21 -0600

    added the survey designs

--- code/20191213_table1_analysis.R
+++ code/20191213_table1_analysis.R
@@ -21,18 +21,46 @@
 #conditions are for sample adults
 #others are demographic and use PERWEIGHT
 
-#make four survey designs: one for sampweight, one for perweight, one for
+#make five survey designs: two for sampweight, one for perweight, one for
 #mortality weight for 2000-2014, and one for mortality weight 2000-2010
 library(survey)
 library(tableone)
+library(tidyverse)
 eligible <- read.csv("data\\eligible.csv")
 
+#the reason it's 15 and 11 is because 2000-2014 is actually 15 total cycles,
+#and 2000-2010 is 11 cycles!
 eligible<- eligible %>%
   mutate(sampWeight14 = SAMPWEIGHT / 15,
-         sampWeight10 = SAMPWEIGHT / 11)
-eligible$samp10 <- 
-  samp.Svy <- svydesign(ids = ~ PSU, strata = ~ STRATA, weights = ~ (SAMPWEIGHT/14),
+         sampWeight10 = SAMPWEIGHT / 11,
+         sampWeight5 = SAMPWEIGHT / 5)
+
+eligible<- eligible %>%
+  mutate(perWeight14 = PERWEIGHT / 15,
+         mortWeight14 = MORTWTSA / 15,
+         mortWeight10 = MORTWTSA / 11,
+         mortWeight5 = MORTWTSA / 5)
+
+#sample adult weights. Need 1 for vars assesed in all years, another
+#for those assessed in 2010-2014 (the more descriptive CRN items)
+samp14.Svy <- svydesign(ids = ~ PSU, strata = ~ STRATA, weights = ~ sampWeight14,
                         nest = TRUE, data = eligible)
+
+samp5.Svy <- svydesign(ids = ~ PSU, strata = ~ STRATA, weights = ~ sampWeight5,
+                        nest = TRUE, data = eligible)
+
+#person weights, for demographic variables
+per14.Svy <- svydesign(ids = ~ PSU, strata = ~ STRATA, weights = ~ perWeight14,
+                       nest = TRUE, data = eligible)
+
+#mortality weights, for COXPH
+mort14.Svy <- svydesign(ids = ~ PSU, strata = ~ STRATA, weights = ~ mortWeight14,
+                        nest = TRUE, data = eligible)
+
+mort10.Svy <- svydesign(ids = ~ PSU, strata = ~ STRATA, weights = ~ mortWeight10,
+                        nest = TRUE, data = eligible)
+
+
 
 
 svyCreateTableOne2(vars = c("HI_CHOL","race","agecat","RIAGENDR"),
