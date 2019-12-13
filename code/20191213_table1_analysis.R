6889733560fca18d8b37744c6a8b76c645e896f2
sarahVanAlsten <sarah.vanalsten@wustl.edu>
2019-12-13 16:47:13 -0600

    recoded barrier to care variables

--- code/20191213_table1_analysis.R
+++ code/20191213_table1_analysis.R
@@ -26,7 +26,61 @@
 library(survey)
 library(tableone)
 library(tidyverse)
+
+#read in the data
 eligible <- read.csv("data\\eligible.csv")
+#create a CRN measure
+#recode delayed medical care due to cost, and other reasons
+eligible <- eligible %>%
+  mutate(DELAYCOSTR = ifelse(DELAYCOST > 2 | DELAYCOST == 0, NA, 
+                             ifelse(DELAYCOST == 1, 0, 1))) %>% #care cost too much
+  mutate(DELAYAPPTR = ifelse(DELAYAPPT > 2 | DELAYAPPT == 0, NA, 
+                             ifelse(DELAYAPPT == 1, 0, 1))) %>% #couldn't get appt soon enough
+  mutate(DELAYHRSR = ifelse(DELAYHRS > 2 | DELAYHRS == 0, NA, 
+                            ifelse(DELAYHRS == 1, 0, 1))) %>% #office hours didn't work
+  mutate(DELAYPHONER = ifelse(DELAYPHONE > 2 | DELAYPHONE == 0, NA, 
+                              ifelse(DELAYPHONE == 1, 0, 1))) %>% #couldn't reach by phone
+  mutate(DELAYTRANSR = ifelse(DELAYTRANS > 2 | DELAYTRANS == 0, NA, 
+                              ifelse(DELAYTRANS == 1, 0, 1))) %>% #couldn't get transportation
+  mutate(DELAYWAITR = ifelse(DELAYWAIT > 2 | DELAYWAIT == 0, NA, 
+                             ifelse(DELAYWAIT == 1, 0, 1))) %>% #wait time too long
+  mutate(BarrierCareR = ifelse(YBARCARE > 2 | YBARCARE == 0, NA, 
+                               ifelse(YBARCARE == 1, 0, 1))) %>% #needed but couldn't afford med care
+  mutate(BarrierMedR = ifelse(YBARMEDS > 2 | YBARMEDS == 0, NA, 
+                              ifelse(YBARMEDS == 1, 0, 1)))%>%  #needed but couldn't afford medication
+  mutate(BarrierFUR = ifelse(YBARFOLLOW > 2 | YBARFOLLOW == 0, NA, 
+                             ifelse(YBARFOLLOW == 1, 0, 1)))%>%  #needed but couldn't afford followup
+  mutate(BarrierSpecR = ifelse(YBARSPECL > 2 | YBARSPECL == 0, NA, 
+                               ifelse(YBARSPECL == 1, 0, 1)))%>%  #needed but couldn't afford specialist
+  mutate(BarrierMHR = ifelse(YBARMENTAL > 2 | YBARMENTAL == 0, NA, 
+                             ifelse(YBARMENTAL == 1, 0, 1))) #needed but couldn't afford mental health care
+#Behaviors to Save Money on Meds
+eligible <- eligible %>%
+  mutate(skipMed = ifelse(YSKIPMEDYR > 2 | YSKIPMEDYR == 0, NA,
+                          ifelse(YSKIPMEDYR == 1, 0,
+                                 ifelse(YSKIPMEDYR ==2, 1,NA))))%>%
+  mutate(delayMed = ifelse(YDELAYMEDYR > 2| YDELAYMEDYR == 0, NA,
+                           ifelse(YDELAYMEDYR == 1, 0,
+                                  ifelse(YDELAYMEDYR ==2, 1,NA))))%>%
+  mutate(CheapMed = ifelse(YCHEAPMEDYR > 2 | YCHEAPMEDYR == 0, NA,
+                           ifelse(YCHEAPMEDYR == 1, 0,
+                                  ifelse(YCHEAPMEDYR ==2, 1,NA))))%>%
+  mutate(foreignMed = ifelse(YFORNMEDYR > 2 | YFORNMEDYR == 0, NA,
+                             ifelse(YFORNMEDYR == 1, 0,
+                                    ifelse(YFORNMEDYR ==2, 1,NA))))%>%
+  mutate(alternateMed = ifelse(YALTMEDYR > 2 | YALTMEDYR == 0, NA,
+                               ifelse(YALTMEDYR == 1, 0,
+                                      ifelse(YALTMEDYR ==2, 1,NA))))%>%
+  mutate(lessMed = ifelse(YSKIMPMEDYR > 2 | YSKIMPMEDYR == 0, NA,
+                          ifelse(YSKIMPMEDYR == 1, 0,
+                                 ifelse(YSKIMPMEDYR ==2, 1,NA))))
+
+
+#yes if ybarmedr is as yes or any of the the 3 specific measures are a yes
+eligible <- eligible %>%
+  mutate(CRN = ifelse(BarrierMedR == 1 | skipMed == 1 | lessMed == 1 | delayMed == 1, 1, 
+                      ifelse(is.na(BarrierMedR), NA, 0)))
+table(eligible$CRN, useNA = "ifany")
 
 #the reason it's 15 and 11 is because 2000-2014 is actually 15 total cycles,
 #and 2000-2010 is 11 cycles!
@@ -60,16 +114,53 @@ mort14.Svy <- svydesign(ids = ~ PSU, strata = ~ STRATA, weights = ~ mortWeight14
 mort10.Svy <- svydesign(ids = ~ PSU, strata = ~ STRATA, weights = ~ mortWeight10,
                         nest = TRUE, data = eligible)
 
+####################################################################
+#per survey package guidelines, use subset() to get appropriate subpopulation estimates
+diab.samp14 <- subset(samp14.Svy, DiabetesRec == 1)
+diab.samp5 <- subset(samp5.Svy, DiabetesRec == 1)
+cvd.samp14 <- subset(samp14.Svy, AnyCVD == 1)
+cvd.samp5 <- subset(samp5.Svy, AnyCVD == 1)
+cvdht.samp14 <- subset(samp14.Svy, AnyCVDHT == 1)
+cvdht.samp5 <- subset(samp5.Svy, AnyCVDHT == 1)
 
+diab.per14 <- subset(per14.Svy, DiabetesRec == 1)
+cvd.per14 <- subset(per14.Svy, AnyCVD == 1)
+cvdht.per14 <- subset(per14.Svy, AnyCVDHT == 1)
 
+diab.mort14 <- subset(mort14.Svy, DiabetesRec == 1)
+cvd.mort14 <- subset(mort14.Svy, AnyCVD == 1)
+cvdht.mort14 <- subset(mort14.Svy, AnyCVDHT == 1)
 
-svyCreateTableOne2(vars = c("HI_CHOL","race","agecat","RIAGENDR"),
-                   strata = "RIAGENDR", data = nhanesSvy)
+diab.mort10 <- subset(mort10.Svy, DiabetesRec == 1)
+cvd.mort10 <- subset(mort10.Svy, AnyCVD == 1)
+cvdht.mort10 <- subset(mort10.Svy, AnyCVDHT == 1)
+###############################################################################
+#clean up environment to help things run faster
+rm(eligible)
+rm(per14.Svy)
+rm(mort10.Svy)
+rm(mort14.Svy)
+rm(samp5.Svy)
+rm(samp14.Svy)
+######################################################################################
+#now, get descriptive statistics
+#FIRST, for CRN bx assessed in late years
+svyCreateTableOne(vars = c("skipMed", "delayMed", "lessMed"), strata = 'CRN', data = diab.samp5, 
+                  factorVars = c("skipMed", "delayMed", "lessMed"), includeNA = FALSE,
+                  test = TRUE, smd = TRUE)
+svyCreateTableOne(vars = c("skipMed", "delayMed", "lessMed"), strata = 'CRN', data = cvd.samp5, 
+                  factorVars = c("skipMed", "delayMed", "lessMed"), includeNA = FALSE,
+                  test = TRUE, smd = TRUE)
+svyCreateTableOne(vars = c("skipMed", "delayMed", "lessMed"), strata = 'CRN', data = cvdht.samp5, 
+                  factorVars = c("skipMed", "delayMed", "lessMed"), includeNA = FALSE,
+                  test = TRUE, smd = TRUE)
 
-tableone::svyCreateTableOne(vars, strata = STRATA, data = eligible, 
-                            factorVars, includeNA = FALSE,
-                            test = TRUE, testApprox = svyTestChisq, argsApprox = NULL,
-                            testNormal = svyTestNormal, argsNormal = list(method = "Wald"),
-                            testNonNormal = svyTestNonNormal, argsNonNormal = NULL, smd = TRUE)
-
-
+svyCreateTableOne(vars = c("BarrierMedR", "SmokeR"), strata = 'CRN', data = diab.samp14, 
+                  factorVars = c("BarrierMedR", "SmokeR"), includeNA = FALSE,
+                  test = TRUE, smd = TRUE)
+svyCreateTableOne(vars = c("BarrierMedR", "SmokeR"), strata = 'CRN', data = cvd.samp14, 
+                  factorVars = c("BarrierMedR", "SmokeR"), includeNA = FALSE,
+                  test = TRUE, smd = TRUE)
+svyCreateTableOne(vars = c("BarrierMedR", "SmokeR"), strata = 'CRN', data = cvdht.samp14, 
+                  factorVars = c("BarrierMedR", "SmokeR"), includeNA = FALSE,
+                  test = TRUE, smd = TRUE)
