b1a92c3e7d57b545835d942149d6a59cd0855ccc
sarahVanAlsten <sarah.vanalsten@wustl.edu>
2020-03-03 15:08:43 -0600

    Incorporated writing edits from JKH

--- code/20191216_psus_strata.R
+++
@@ -1,8 +0,0 @@
-##################################################################
-#Sarah Van Alsten
-#Created: Dec 16, 2019
-#Get the strata and PSUs from CDC directly, problem might be IPUMS constructed variables
-#Packages used: ipumsr, tidyverse, tableone, survival, survey
-#Last Update: Dec 16, 2019
-################################################################################
-
